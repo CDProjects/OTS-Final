996309e4fc360cfbd90904b3022d49d64b501e45
CDProjects <caseydentprojects@gmail.com>
2024-08-18 22:39:40 +0300

    Line breaks for articles resolved

--- src/Components/Article.js
+++ src/Components/Article.js
@@ -1,32 +1,100 @@
-import React from 'react';
+import React from "react";
 
 const Article = () => {
   return (
     <div className="article">
-      <h2>Shamrocksilla menee erinomaisesti, ja joukkue taistelee Rugbyn SM-kullasta tänä vuonna.</h2>
-      <p>Old Town Shamrocks matkusti viikonloppuna Tampereelle pelaamaan erittäin tärkeän ottelun Rugbyn SM-sarjassa. Tampere Rugby Club aloitti pelin vahvasti, mutta OTS osoitti jälleen kerran sinnikkyyttä ja päättäväisyyttä. Shamrocks käänsi ottelun edukseen toisella puoliajalla ja vei lopulta voiton lukemin 32–7. Tampere-pelin maaleista vastasivat Jay Robertson ja Widz Rahui, jotka molemmat tekivät kaksi maalia. Lisäksi varakapteeni Samu Pääkkö ja kapteeni Akseli Peltoniemi pääsivät myös maalin makuun. Alexander Bannatyne viimeisteli yhden onnistuneen lisäpistepotkun.</p>
-      
-      <p>Old Town Shamrocksin (OTS) kausi on ollut vahva, ja joukkue on tällä hetkellä sarjataulukon toisella sijalla, kun jäljellä on enää kolme ottelua. Kausi on ollut täynnä määrätietoisuutta, kovia voittoja ja muutamia haasteita, mutta Shamrocks on osoittanut olevansa varteenotettava voima. Joukkue on kerännyt 24 pistettä. Warriors Rugby club on ainoa joukkue joka on voittanut Shamrocksin. Shamrocksin tasainen suorittaminen on asettanut joukkueen loistavaan asemaan kauden loppusuoraa ajatellen.</p>
-      
+      <h2>
+        Shamrocksilla menee erinomaisesti, ja joukkue taistelee Rugbyn
+        SM-kullasta tänä vuonna.
+      </h2>
+      <p>
+        Old Town Shamrocks matkusti viikonloppuna Tampereelle pelaamaan erittäin
+        tärkeän ottelun Rugbyn SM-sarjassa. Tampere Rugby Club aloitti pelin
+        vahvasti, mutta OTS osoitti jälleen kerran sinnikkyyttä ja
+        päättäväisyyttä. Shamrocks käänsi ottelun edukseen toisella puoliajalla
+        ja vei lopulta voiton lukemin 32–7. Tampere-pelin maaleista vastasivat
+        Jay Robertson ja Widz Rahui, jotka molemmat tekivät kaksi maalia.
+        Lisäksi varakapteeni Samu Pääkkö ja kapteeni Akseli Peltoniemi pääsivät
+        myös maalin makuun. Alexander Bannatyne viimeisteli yhden onnistuneen
+        lisäpistepotkun.
+        <br />
+        <br />
+        Old Town Shamrocksin (OTS) kausi on ollut vahva, ja joukkue on tällä
+        hetkellä sarjataulukon toisella sijalla, kun jäljellä on enää kolme
+        ottelua. Kausi on ollut täynnä määrätietoisuutta, kovia voittoja ja
+        muutamia haasteita, mutta Shamrocks on osoittanut olevansa
+        varteenotettava voima. Joukkue on kerännyt 24 pistettä. Warriors Rugby
+        club on ainoa joukkue joka on voittanut Shamrocksin. Shamrocksin
+        tasainen suorittaminen on asettanut joukkueen loistavaan asemaan kauden
+        loppusuoraa ajatellen.
+      </p>
+
       {/* Add the rest of the Finnish text here */}
-      
+
       <h3>English:</h3>
-      <p>Shamrocks is doing exceptionally well and is competing for the Finnish Rugby Championship gold this year.</p>
-      
-      <p>Old Town Shamrocks traveled to Tampere over the weekend to play a crucial match in the Finnish Rugby Championship. Tampere Rugby Club started the game strong, but OTS once again demonstrated resilience and determination. Shamrocks turned the game in their favor in the second half and ultimately secured a 32-7 victory. The try scorers in the Tampere game were Jay Robertson and Widz Rahui, who both scored twice. Additionally, Vice-Captain Samu Pääkkö and Captain Akseli Peltoniemi also found the try zone. Alexander Bannatyne successfully converted one extra point.
-<br/>
-<br/>
-Old Town Shamrocks (OTS) has had a strong season and currently sits in second place on the table with only three games remaining. The season has been marked by determination, hard-fought victories, and a few challenges, but Shamrocks has proven to be a formidable force. The team has accumulated 24 points, with Warriors Rugby Club being the only team to have defeated Shamrocks. Consistent performance has put Shamrocks in an excellent position as the season heads towards its conclusion. The next three matches will be crucial for the playoffs. First, OTS will travel to Tallinn to face Kalev, followed by an away game against Turku. The season will conclude with a home match against Kalev. Securing second place in the standings is of utmost importance for OTS, as it would guarantee a home semi-final against the team finishing third. Shamrocks understands the significance of home-field advantage, especially in a high-stakes semi-final, and the team is determined to claim that spot. Meanwhile, the league winner advances directly to the final.
-<br/>
-<br/>
-The success of this season is attributed to coaches Marko Sallert and Linus Backman, who have managed to instill a unified and, above all, disciplined style of play in the team. Under their leadership, Shamrocks has developed throughout the season, maintaining focus and performance even in tough situations.
-<br/>
-<br/>
-This year’s finals day will be held in Espoo and is fast approaching. Shamrocks is firmly focused on maintaining consistent performance, as it is key to a strong finish to the season. To secure a home semi-final, every match counts, and the team is preparing to give its all in the upcoming challenges. The season is nearing its end, and OTS fans have plenty of reasons to be excited. The team is in a strong position, and with a few solid performances, Shamrocks could be playing a home semi-final and contending for the championship. The road ahead is challenging, but Shamrocks has already shown that it has the determination and discipline needed to overcome any obstacle. All eyes are now on the upcoming matches as OTS continues its journey towards the championship.
-<br/>
-<br/>
-The team will travel to Tallinn on Saturday to play against Kalev. Even a single point from that match would secure a place in the semi-finals on September 21st. The Finnish Championship finals will be played on September 28th in Leppävaara, Espoo. In addition to the playoffs and the women’s and men’s Finnish Championship finals, Shamrocks’ U8 and U13 juniors will also be playing on finals day.</p>
-      
+      <p>
+        Shamrocks is doing exceptionally well and is competing for the Finnish
+        Rugby Championship gold this year.
+        <br />
+        <br />
+        Old Town Shamrocks traveled to Tampere over the weekend to play a
+        crucial match in the Finnish Rugby Championship. Tampere Rugby Club
+        started the game strong, but OTS once again demonstrated resilience and
+        determination. Shamrocks turned the game in their favor in the second
+        half and ultimately secured a 32-7 victory. The try scorers in the
+        Tampere game were Jay Robertson and Widz Rahui, who both scored twice.
+        Additionally, Vice-Captain Samu Pääkkö and Captain Akseli Peltoniemi
+        also found the try zone. Alexander Bannatyne successfully converted one
+        extra point.
+        <br />
+        <br />
+        Old Town Shamrocks (OTS) has had a strong season and currently sits in
+        second place on the table with only three games remaining. The season
+        has been marked by determination, hard-fought victories, and a few
+        challenges, but Shamrocks has proven to be a formidable force. The team
+        has accumulated 24 points, with Warriors Rugby Club being the only team
+        to have defeated Shamrocks. Consistent performance has put Shamrocks in
+        an excellent position as the season heads towards its conclusion. The
+        next three matches will be crucial for the playoffs. First, OTS will
+        travel to Tallinn to face Kalev, followed by an away game against Turku.
+        The season will conclude with a home match against Kalev. Securing
+        second place in the standings is of utmost importance for OTS, as it
+        would guarantee a home semi-final against the team finishing third.
+        Shamrocks understands the significance of home-field advantage,
+        especially in a high-stakes semi-final, and the team is determined to
+        claim that spot. Meanwhile, the league winner advances directly to the
+        final.
+        <br />
+        <br />
+        The success of this season is attributed to coaches Marko Sallert and
+        Linus Backman, who have managed to instill a unified and, above all,
+        disciplined style of play in the team. Under their leadership, Shamrocks
+        has developed throughout the season, maintaining focus and performance
+        even in tough situations.
+        <br />
+        <br />
+        This year’s finals day will be held in Espoo and is fast approaching.
+        Shamrocks is firmly focused on maintaining consistent performance, as it
+        is key to a strong finish to the season. To secure a home semi-final,
+        every match counts, and the team is preparing to give its all in the
+        upcoming challenges. The season is nearing its end, and OTS fans have
+        plenty of reasons to be excited. The team is in a strong position, and
+        with a few solid performances, Shamrocks could be playing a home
+        semi-final and contending for the championship. The road ahead is
+        challenging, but Shamrocks has already shown that it has the
+        determination and discipline needed to overcome any obstacle. All eyes
+        are now on the upcoming matches as OTS continues its journey towards the
+        championship.
+        <br />
+        <br />
+        The team will travel to Tallinn on Saturday to play against Kalev. Even
+        a single point from that match would secure a place in the semi-finals
+        on September 21st. The Finnish Championship finals will be played on
+        September 28th in Leppävaara, Espoo. In addition to the playoffs and the
+        women’s and men’s Finnish Championship finals, Shamrocks’ U8 and U13
+        juniors will also be playing on finals day.
+      </p>
+
       {/* Add the rest of the English text here */}
     </div>
   );
