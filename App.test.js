6d673cb61c68d3d03bb61fd7a969bb60a2fd5b5b
CDProjects <caseydentprojects@gmail.com>
2024-10-17 11:36:48 +0300

    App.test.js updated

--- App.test.js
+++ App.test.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+// Mock the window.scrollTo function to avoid errors in the test environment
+beforeAll(() => {
+  window.scrollTo = jest.fn();
+});
+
 test('renders team member image', () => {
   // Render the App component
   render(<App />);
